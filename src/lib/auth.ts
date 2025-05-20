5e1e84237d2733f760392637b3c5cd7e6f9d5d0b
Rajat12826 <pariharrajat078@gmail.com>
2025-05-20 09:49:33 +0530

    A

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,4 +9,4 @@ export const login = async (data: { email: string; password: string }) => {
   return axios.post("/api/auth/signin", data);
 };
 
-export const axiosServices = axios.create({ baseURL: "http://localhost:3000/api/", withCredentials:true });
+export const axiosServices = axios.create({ baseURL: "https://rajat-blogs-three.vercel.app/api", withCredentials:true });
